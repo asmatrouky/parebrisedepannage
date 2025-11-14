0555c31b46e322cf9b47c4a72add191e90d05579
Asmatrkk <atrouky94@gmail.com>
2025-11-14 23:58:23 +0100

    Fix chemin image hero + CSS

--- app/layout.js
+++ app/layout.js
@@ -15,6 +15,14 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="fr">
+      <head>
+        <link 
+          rel="stylesheet" 
+          href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css" 
+          crossOrigin="anonymous" 
+          referrerPolicy="no-referrer" 
+        />
+      </head>
       <body className={poppins.className}>
         <Navbar />
         <main>{children}</main>
