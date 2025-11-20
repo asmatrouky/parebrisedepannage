888880fb0934c738e94ae7bc0cc04ab076ab83e2
Asmatrkk <atrouky94@gmail.com>
2025-11-20 13:13:44 +0100

    Mise à jour css mobile

--- components/hero.jsx
+++ components/hero.jsx
@@ -19,7 +19,7 @@ export default function Hero() {
             </div>
 
             <h1>
-              Remplacement de pare-brise rapide sans rien avancer
+              Votre pare-brise cassé ? <br />On s’occupe de tout…
             </h1>
 
             <p>
