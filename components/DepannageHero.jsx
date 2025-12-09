a0c56f8dfebd5bda2c0b483f499a1f5cd8c8e830
Asmatrkk <atrouky94@gmail.com>
2025-12-09 16:31:04 +0100

    mise à jour css

--- components/DepannageHero.jsx
+++ components/DepannageHero.jsx
@@ -135,7 +135,7 @@ export default function DepannageHero() {
                   onChange={handleChange}
                   value={formData.date}
                   required
-                  inputMode="numeric"
+                  inputMode="date"
                 />
               ) : (
                 <input
